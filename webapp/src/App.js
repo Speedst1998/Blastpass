78485c398e68ee373ce41f70bf778445aaf02463
Richard Lei <46201873+richcs@users.noreply.github.com>
2023-02-18 14:41:09 -0500

    form page

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import logo from './blastpass-logo.svg';
 import './App.css';
+import React from 'react';
+import { TextField, Button } from '@mui/material';
+import { spacing } from '@mui/system';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class MyApp extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = this.getDefaultState();
+  }
 
-export default App;
+  getDefaultState = () => {
+    return { website: '', username: '', password: '' };
+  }
+
+  clear = () => {
+    this.setState(this.getDefaultState())
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+
+        </header>
+        <div className='App-body'>
+        <TextField id="standard-basic"  value={this.state.website} onChange={(e)=>{this.setState({website: e.target.value})}} label="Website" variant="standard" sx={{ m: 1 }}/>
+          <TextField id="standard-basic" label="User Name" value={this.state.username} onChange={(e)=>{this.setState({username: e.target.value})}} variant="standard" sx={{ m: 1 }}/>
+          <TextField id="standard-basic" label="Password" value={this.state.password} onChange={(e)=>{this.setState({password: e.target.value})}} variant="standard" sx={{ m: 1 }}/>
+          <Button variant="contained" color="error" onClick={this.clear} sx={{ m: 5 }}>Safely Save</Button>
+          
+          <a
+            className="App-link"
+            href="https://www.reddit.com/r/BlastPass/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Reddit
+          </a>
+        </div>
+      </div>
+    );
+  }
+};
+
+export default MyApp
