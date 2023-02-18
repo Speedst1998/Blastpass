4cf7c1150e961fdd277000b1e7c0514babf478e6
Jin Qi <jenny_qiqu@hotmail.com>
2023-02-18 14:31:43 -0500

    Added post route

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const { body, validationResult } = require('express-validator');
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.listen(port);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.use(express.json());
+
+app.post(
+  '/',
+  body('username', 'Username is required').trim().not().isEmpty(),
+  body('password', 'Password is required').trim().not().isEmpty(),
+  body('website', 'Website is required').trim().not().isEmpty(),
+  (req, res) => {
+    try {
+      validationResult(req).throw();
+      // call post to reddit function
+      res.send("good");
+    } catch (err) {
+      res.status(400).send("bad");
+    }
+});
